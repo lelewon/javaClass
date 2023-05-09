c5c15a536ab987272851713e8b2d19ff2f8e306e
Administrator <Administrator@DESKTOP-N8J5NFQ>
2023-05-09 18:13:06 +0900

    5월 9일 수업

--- j0509_01/src/j0509_01/Classj0509_11.java
+++ j0509_01/src/j0509_01/Classj0509_11.java
@@ -1,12 +1,35 @@
 package j0509_01;
 
+import java.util.Scanner;
+
 public class Classj0509_11 {
 
 	public static void main(String[] args) {
+		// 실수를 입력받아 소수점 4자리에서 올림해서 출력하시오
+		double input = 0;
+		Scanner scan = new Scanner(System.in);
+		System.out.println("실수를 입력하세요");
+		input = scan.nextDouble();
+		double result = Math.ceil(input*1000)/1000;
+		System.out.println(result);		
+		
+		
+		
+		// 소수점 5자리를 입력받아 3자리에서 반올림해서 출력하세요
+//		double num = 0;
+//		Scanner scan = new Scanner(System.in);
+//		System.out.println("소수점 5자리의 숫자를 입력하세요");
+//		num = scan.nextDouble();
+//		//반올림 round(), 올림 ceil(), 버림 floor()
+//		double result = Math.round(num*100)/100.0;
+//		System.out.println(result);
+		
+		
+		
 		// double pi = 3.7894; 소수점 2자리에서 반올림해서 출력하세요
-		double pi = 3.7894;
-		double result = Math.round(pi*10)/10.0;
-		System.out.println(result);
+//		double pi = 3.7894;
+//		double result = Math.round(pi*10)/10.0;
+//		System.out.println(result);
 		
 		
 		
